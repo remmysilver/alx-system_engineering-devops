01f7ada64622914614f199ff68982261874152e8
remmysilver <silverremmy@gmail.com>
2023-03-24 04:03:49 -0700

    correction on file 0 and 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,15 +1,5 @@
-# Download flask 2.1.0
-exec { 'apt-get update':
-  command => '/usr/bin/apt-get update'
-}
+# install flask -v
 
-package { 'flask2.1.0':
-  ensure  => 'installed',
-  require => Exec['apt-get update']
-}
-
-package { 'puppet-lint':
-  ensure   => '2.1.0'
-  require  => Package['flask2.1.0'],
-  provider => 'gem'
+exec { 'flask':
+  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
 }
