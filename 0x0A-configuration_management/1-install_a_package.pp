fa265f9bed4b87b3ba28062a909655f60b377787
remmysilver <silverremmy@gmail.com>
2023-03-24 04:05:16 -0700

    correction on file  1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,5 @@
-# install flask -v
-
-exec { 'flask':
-  command => '/usr/bin/apt-get -y install flask -v 2.1.0',
-}
+# install flask from pip3
+package { 'flask' :
+    ensure   => '2.1.0',
+    provider => 'pip3',
+    }
